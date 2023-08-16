639bf45f837538269cebef77fcef669fb19c8f04
Nitesh <nitesh@fireshark.in>
2023-08-16 16:11:01 +0530

    PYTHON CODE

---
+++ 1_Sum.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("code by nitesh")
+# Calculate the sum
+sum_result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
---
+++ 2_subtraction.py
@@ -0,0 +1,11 @@
+# Get input from the user
+AB = int(input("Enter the first number: "))
+BC = int(input("Enter the second number: "))
+CD = int(input("Enter the third number: "))
+# Calculate the subtraction
+subtraction_result = AB - BC - CD 
+
+# Display the result
+print("The subtraction of", AB, "and", BC, CD, "is:", subtraction_result)
+
+print("CODE BYE NITESH")
